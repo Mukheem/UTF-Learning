bee93a470e3d3dc5c18db018e521eede5c91b662
Abdul Mukheem Shaik <absh8989@student.su.se>
2026-08-02 20:57:47 +0200

    First small test for cube spinning added and working fine

---
+++ Assets/Scripts/SpinningCube.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SpinningCube : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2;
+ 
+    protected void Awake ()
+    {
+      // Called: First. Usage: Set up local scope
+    }
+ 
+    protected void Start ()
+    {
+      // Called: Second. Usage: Set up dependencies
+    }
+ 
+    protected void Update ()
+    {
+      // Called: Every frame.
+      transform.Rotate(new Vector3(0, _speed, 0));
+    }
+}
